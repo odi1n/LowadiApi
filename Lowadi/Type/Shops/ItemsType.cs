7bcf883a82a3b5ab52e17d29ef878eba0baf740e
odi1n <sashf1999@mail.ru>
2022-06-04 21:02:58 +0300

    Добавил Description для ItemsType

--- Lowadi/Type/Shops/ItemsType.cs
+++ Lowadi/Type/Shops/ItemsType.cs
@@ -1,115 +1,30 @@
-﻿namespace Lowadi.Models.Type.Shops
+﻿using System.ComponentModel;
+
+namespace Lowadi.Models.Type.Shops
 {
     public enum ItemsType
     {
-        /// <summary>
-        /// Овес
-        /// </summary>
-        Oats,
-
-        /// <summary>
-        /// Фураж
-        /// </summary>
-        Forage,
-
-        /// <summary>
-        /// Яблоко
-        /// </summary>
-        Apple,
-
-        /// <summary>
-        /// Морковь
-        /// </summary>
-        Carrot,
-
-        /// <summary>
-        /// Комбикорм
-        /// </summary>
-        CompoundFeed,
-
-        /// <summary>
-        /// Дерево
-        /// </summary>
-        Wood,
-
-        /// <summary>
-        /// Iron
-        /// </summary>
-        Iron,
-
-        /// <summary>
-        /// Песок
-        /// </summary>
-        Sand,
-
-        /// <summary>
-        /// Кожа
-        /// </summary>
-        Leather,
-
-        /// <summary>
-        /// Солома
-        /// </summary>
-        Straw,
-
-        /// <summary>
-        /// Лен
-        /// </summary>
-        Flax,
-
-        /// <summary>
-        /// Пшеница
-        /// </summary>
-        Wheat,
-
-        /// <summary>
-        /// Семена льна
-        /// </summary>
-        SeedsFlax,
-
-        /// <summary>
-        /// Семена яблок
-        /// </summary>
-        SeedsApple,
-
-        /// <summary>
-        /// Семена люцерны
-        /// </summary>
-        SeedsAlfalfa,
-
-        /// <summary>
-        /// Семена овса
-        /// </summary>
-        SeedsOat,
-
-        /// <summary>
-        /// Семена пшеницы
-        /// </summary>
-        SeedsWheat,
-
-        /// <summary>
-        /// Семена маркови
-        /// </summary>
-        SeedsCarrot,
-
-        /// <summary>
-        /// Семена пропуска
-        /// </summary>
-        SeedsPass,
-
-        /// <summary>
-        /// Удобрение 1
-        /// </summary>
-        Fertilizer_1,
-
-        /// <summary>
-        /// Удобрение 2
-        /// </summary>
-        Fertilizer_2,
-
-        /// <summary>
-        /// Навоз
-        /// </summary>
-        Manure,
+        [Description("Овис")] Oats,
+        [Description("Фураж")] Forage,
+        [Description("Яблоко")] Apple,
+        [Description("Морковь")] Carrot,
+        [Description("Комбикорм")] CompoundFeed,
+        [Description("Дерево")] Wood,
+        [Description("Железо")] Iron,
+        [Description("Песок")] Sand,
+        [Description("Кожа")] Leather,
+        [Description("Солома")] Straw,
+        [Description("Лен")] Flax,
+        [Description("Пшеница")] Wheat,
+        [Description("Семена льна")] SeedsFlax,
+        [Description("Семена яблок")] SeedsApple,
+        [Description("Семена люцерны")] SeedsAlfalfa,
+        [Description("Семена овса")] SeedsOat,
+        [Description("Семена пшеницы")] SeedsWheat,
+        [Description("Семена маркови")] SeedsCarrot,
+        [Description("Семена пропуска")] SeedsPass,
+        [Description("Удобрение 1")] Fertilizer_1,
+        [Description("Удобрение 2")] Fertilizer_2,
+        [Description("Навоз")] Manure,
     }
 }
